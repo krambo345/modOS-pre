95bbcf72ea55860a18875fbb4c55eb4ae9011aa6
Huseyin <krambo345@gmail.com>
2026-08-12 05:32:17 -0600

    Fix icon path for desktop icons

--- base/system/exec.js
+++ base/system/exec.js
@@ -9,7 +9,7 @@ async function makeDesktop() {
         if (app.visible || visibility) {
             desktop.insertAdjacentHTML("beforeend", `
         <div class="desktop-icon" ondblclick="exec('${name}')">
-            <img src="/modOS/base/system/icons/${app.icon}.png">
+            <img src="/modOS-pre/base/system/icons/${app.icon}.png">
             <span>${name}</span>
         </div>
     `);
